5d4cca7bc2f85f65cab60e756074151b2f3b9d9a
Vineetha Yenugula <vineethay96@gmail.com>
2020-05-01 13:55:01 -0500

    added compression librarry to middleware chain

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter); 
+
 dotenv.config({ path: '.env' });
 
 //Set up mongoose connection
