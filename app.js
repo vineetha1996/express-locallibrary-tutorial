cd6461cb5621515fe4ad4dd82a0699439d6181bf
vineetha1996 <59989572+vineetha1996@users.noreply.github.com>
2020-05-05 15:55:02 -0500

    Update app.js
    added my Atlas username and password.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var dev_db_url = 'mongodb+srv://vineethay18@gmail.com:vinni@96@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
